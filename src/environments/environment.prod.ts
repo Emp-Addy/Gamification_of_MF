8c01ca5052cb7fac5a826b549826216a2e23822f
Aditya Shende <aditya_shende1@persistent.com>
2023-02-24 19:59:54 +0530

    all files added.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBsh-df89QYr8rWxDK3XAleaPQNaj0T4gs",
+    authDomain: "gamification-mf.firebaseapp.com",
+    projectId: "gamification-mf",
+    storageBucket: "gamification-mf.appspot.com",
+    messagingSenderId: "185628817170",
+    appId: "1:185628817170:web:de698d03ad136c4b7425cb",
+    measurementId: "G-53C0S8DXHM"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBsh-df89QYr8rWxDK3XAleaPQNaj0T4gs",
+    authDomain: "gamification-mf.firebaseapp.com",
+    projectId: "gamification-mf",
+    storageBucket: "gamification-mf.appspot.com",
+    messagingSenderId: "185628817170",
+    appId: "1:185628817170:web:de698d03ad136c4b7425cb",
+    measurementId: "G-53C0S8DXHM"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
